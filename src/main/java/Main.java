b8cb37e5b6a7ebdfb181cca3a4d3e31d121591cf
George Foot (Gobogames) <George.Foot@gobogames.co.uk>
2013-08-29 18:57:09 +0100

    basic 'hello world' resource as a test

---
+++ src/main/java/Main.java
@@ -0,0 +1,37 @@
+import com.sun.grizzly.http.SelectorThread;
+import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: george
+ * Date: 29/08/13
+ * Time: 18:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        final String baseUri = "http://localhost:9998/";
+        final Map<String, String> initParams =
+                new HashMap<String, String>();
+
+        initParams.put("com.sun.jersey.config.property.packages",
+                "com.studiogobo.fi.matcher.resources");
+
+        System.out.println("Starting grizzly...");
+        SelectorThread threadSelector =
+                GrizzlyWebContainerFactory.create(baseUri, initParams);
+        System.out.println(String.format(
+                "Jersey app started with WADL available at %sapplication.wadl\n" +
+                "Try out %shelloworld\nHit enter to stop it...", baseUri, baseUri));
+        System.in.read();
+        threadSelector.stopEndpoint();
+        System.exit(0);
+    }
+}
+
