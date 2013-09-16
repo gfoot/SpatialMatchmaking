05c90b7057ff1bc00995a36adfe3de0dc0ba3919
George Foot (Gobogames) <George.Foot@gobogames.co.uk>
2013-09-16 18:54:13 +0100

    Move Flow.cs into namespace, and remove optional parameter to let that work

--- Unity/MatchTest/Assets/Flow.cs
+++ Unity/MatchTest/Assets/Flow.cs
@@ -1,104 +1,109 @@
-﻿using Assets;
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-public class Flow : MonoBehaviour
+namespace Assets
 {
-    private System.Guid _uuid;
-    private int _clientId;
-
-    public void Start()
+    public class Flow : MonoBehaviour
     {
-        StartCoroutine(Run());
-    }
+        private System.Guid _uuid;
+        private int _clientId;
 
-    private IEnumerator Run()
-    {
-        _uuid = System.Guid.NewGuid();
-        yield return null;
-    }
-
-    private static void GuiField(string label, string value)
-    {
-        GUILayout.BeginHorizontal();
+        public void Start()
         {
-            GUILayout.Label(label, GUILayout.Width(200));
-            GUILayout.Label(value, GUILayout.ExpandWidth(true));
+            StartCoroutine(Run());
         }
-        GUILayout.EndHorizontal();
-    }
 
-    private static bool GuiButton(string label)
-    {
-        bool value;
-
-        GUILayout.BeginHorizontal();
+        private IEnumerator Run()
         {
-            value = GUILayout.Button(label, GUILayout.ExpandWidth(false));
+            _uuid = System.Guid.NewGuid();
+            yield return null;
         }
-        GUILayout.EndHorizontal();
 
-        return value;
-    }
-
-    public void OnGUI()
-    {
-        GUILayout.BeginArea(new Rect(Screen.width * 0.1f, Screen.height * 0.1f, Screen.width * 0.8f, Screen.height * 0.8f));
+        private static void GuiField(string label, string value)
         {
-            GUILayout.BeginVertical();
+            GUILayout.BeginHorizontal();
             {
-                GuiField("UUID", _uuid.ToString());
+                GUILayout.Label(label, GUILayout.Width(200));
+                GUILayout.Label(value, GUILayout.ExpandWidth(true));
+            }
+            GUILayout.EndHorizontal();
+        }
 
-                if (_clientId == 0)
+        private static bool GuiButton(string label)
+        {
+            bool value;
+
+            GUILayout.BeginHorizontal();
+            {
+                value = GUILayout.Button(label, GUILayout.ExpandWidth(false));
+            }
+            GUILayout.EndHorizontal();
+
+            return value;
+        }
+
+        public void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(Screen.width * 0.1f, Screen.height * 0.1f, Screen.width * 0.8f, Screen.height * 0.8f));
+            {
+                GUILayout.BeginVertical();
                 {
-                    if (GuiButton("Register"))
+                    GuiField("UUID", _uuid.ToString());
+
+                    if (_clientId == 0)
                     {
-                        Register();
+                        if (GuiButton("Register"))
+                        {
+                            Register();
+                        }
+                    }
+                    else
+                    {
+                        GuiField("Client ID", _clientId.ToString());
                     }
                 }
-                else
-                {
-                    GuiField("Client ID", _clientId.ToString());
-                }
+                GUILayout.EndVertical();
             }
-            GUILayout.EndVertical();
+            GUILayout.EndArea();
         }
-        GUILayout.EndArea();
-    }
 
-    private void Register()
-    {
-        _clientId = 1;
+        private void Register()
+        {
+            _clientId = 1;
 
-        var obj = new JsonObject();
-        obj.Set("hello", "world");
-        obj.Set("another", "string");
-        Debug.Log(obj.ToString());
+            var obj = new JsonObject();
+            obj.Set("hello", "world");
+            obj.Set("another", "string");
+            Debug.Log(obj.ToString());
 
-        var array = new JsonArray();
-        array.Add("hello");
-        var index = array.Add(new JsonObject());
-        array.GetObject(index).Set("inner", "object");
-        obj.Set("array", array);
-        Debug.Log(obj);
+            var array = new JsonArray();
+            array.Add("hello");
+            var index = array.Add(new JsonObject());
+            array.GetObject(index).Set("inner", "object");
+            obj.Set("array", array);
+            Debug.Log(obj);
 
-        TestJsonParser("{  }");
-        TestJsonParser("{ \"a\": \"b\" }");
-        TestJsonParser("{ \"a\": \"b\", }", "{ \"a\": \"b\" }");
-        TestJsonParser("{ \"a\": \"b\", \"c\": \"d\" }");
-        TestJsonParser("{ \"a\": { \"b\": \"c\" }, \"d\": { \"e\": \"f\" } }");
-        TestJsonParser("{ \"a\": [ \"b\", [ { \"c\": \"d\" } ] ] }");
-    }
+            TestJsonParser("{  }");
+            TestJsonParser("{ \"a\": \"b\" }");
+            TestJsonParser("{ \"a\": \"b\", }", "{ \"a\": \"b\" }");
+            TestJsonParser("{ \"a\": \"b\", \"c\": \"d\" }");
+            TestJsonParser("{ \"a\": { \"b\": \"c\" }, \"d\": { \"e\": \"f\" } }");
+            TestJsonParser("{ \"a\": [ \"b\", [ { \"c\": \"d\" } ] ] }");
+        }
 
-    private static void TestJsonParser(string s, string expected = null)
-    {
-        if (expected == null)
-            expected = s;
-        var obj = JsonObject.Parse(s);
-        var t = obj.ToString();
-        if (t != expected)
-            Debug.LogError(string.Format("Json parser failed - {0} => {1}, expected {2}", s, t, expected));
-        else
-            Debug.Log("Json parsed OK - " + s);
+        private static void TestJsonParser(string s)
+        {
+            TestJsonParser(s, s);
+        }
+            
+        private static void TestJsonParser(string s, string expected)
+        {
+            var obj = JsonObject.Parse(s);
+            var t = obj.ToString();
+            if (t != expected)
+                Debug.LogError(string.Format("Json parser failed - {0} => {1}, expected {2}", s, t, expected));
+            else
+                Debug.Log("Json parsed OK - " + s);
+        }
     }
 }
